a045b606496c0aa201b1f0e7d16b40aa2cd11413
joachimmathes <joachimmathes@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-04-19 08:50:43 +0000

    TASK: updated md5 values of files in ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/pt_xml2pdf/trunk@46731 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "pt_xml2pdf"
+# Extension Manager/Repository config file for ext "pt_xml2pdf".
 #
-# Auto generated 26-10-2009 12:23
+# Auto generated 11-04-2011 15:16
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"0239";s:12:"ext_icon.gif";s:4:"5837";s:14:"doc/DevDoc.txt";s:4:"cc14";s:14:"doc/manual.sxw";s:4:"a0b6";s:19:"doc/wizard_form.dat";s:4:"a567";s:20:"doc/wizard_form.html";s:4:"72c2";s:30:"doc/samples/xml2pdf_sample.tpl";s:4:"c7de";s:42:"res/class.tx_ptxml2pdf_barcode_code128.php";s:4:"51a8";s:36:"res/class.tx_ptxml2pdf_generator.php";s:4:"e415";s:31:"res/class.tx_ptxml2pdf_main.php";s:4:"1c8d";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"0239";s:12:"ext_icon.gif";s:4:"5837";s:14:"doc/DevDoc.txt";s:4:"e42c";s:14:"doc/manual.sxw";s:4:"a0b6";s:19:"doc/wizard_form.dat";s:4:"a567";s:20:"doc/wizard_form.html";s:4:"72c2";s:30:"doc/samples/xml2pdf_sample.tpl";s:4:"c7de";s:42:"res/class.tx_ptxml2pdf_barcode_code128.php";s:4:"51a8";s:36:"res/class.tx_ptxml2pdf_generator.php";s:4:"e415";s:31:"res/class.tx_ptxml2pdf_main.php";s:4:"8d3e";}',
 	'suggests' => array(
 	),
 );
